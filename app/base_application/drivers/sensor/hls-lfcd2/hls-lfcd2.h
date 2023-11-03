c9416df58883628096e22063d921e3912c27b44e
Clément Laigle <c.laigle@catie.fr>
2023-11-03 16:17:08 +0100

    Add HLS LFCD2 driver

---
+++ app/base_application/drivers/sensor/hls-lfcd2/hls-lfcd2.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2023 CATIE
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_DRIVERS_SENSOR_HLS_LFCD2_H_
+#define ZEPHYR_DRIVERS_SENSOR_HLS_LFCD2_H_
+
+#define HLS_LFCD2_BUF_SIZE 2520
+#define HLS_LFCD2_SYNC_BYTE 0xFA
+
+struct hls_lfcd2_buf {
+    uint16_t intensity;
+    uint16_t distance;
+};
+
+struct hls_lfcd2_config {
+    const struct device *dev;
+};
+
+struct hls_lfcd2_data {
+    const struct device *gpio_dev;
+
+    sensor_trigger_handler_t handler;
+    const struct sensor_trigger *trigger;
+
+    uint8_t buffer[HLS_LFCD2_BUF_SIZE];
+    struct hls_lfcd2_buf hls_lfcd2_buffer[HLS_LFCD2_BUF_SIZE];
+    uint16_t idx;
+
+    uint16_t rpm;
+};
+
+#endif /* ZEPHYR_DRIVERS_SENSOR_HLS_LFCD2_H_ */
