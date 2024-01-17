8a0434a05d66fb96bfb6a58acece46206d91c698
Clément Laigle <c.laigle@catie.fr>
2024-01-17 14:15:29 +0100

    Reformat code

--- app/lidar_example/src/main.c
+++ app/lidar_example/src/main.c
@@ -3,25 +3,25 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <zephyr/kernel.h>
 #include <zephyr/drivers/sensor.h>
+#include <zephyr/kernel.h>
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main, CONFIG_SENSOR_LOG_LEVEL);
 
 int main(void)
 {
-	const struct device *sensor;
+    const struct device *sensor;
 
-	sensor = DEVICE_DT_GET(DT_NODELABEL(lidar0));
-	if (!device_is_ready(sensor)) {
-		LOG_ERR("Sensor not ready");
-		return 0;
-	}
+    sensor = DEVICE_DT_GET(DT_NODELABEL(lidar0));
+    if (!device_is_ready(sensor)) {
+        LOG_ERR("Sensor not ready");
+        return 0;
+    }
 
-	while (1) {
-		k_sleep(K_FOREVER);
-	}
+    while (1) {
+        k_sleep(K_FOREVER);
+    }
 
-	return 0;
+    return 0;
 }
