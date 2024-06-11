c5feedda177ae4290b378df60dea6db08b696ed7
Clément Laigle <c.laigle@catie.fr>
2024-06-11 13:39:16 +0000

    Update LiDAR example

--- app/lidar_example/src/main.c
+++ app/lidar_example/src/main.c
@@ -3,25 +3,273 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/uart.h>
+#include <zephyr/kernel.h>
+
+#include <rcl/error_handling.h>
+#include <rcl/rcl.h>
+#include <rclc/executor.h>
+#include <rclc/rclc.h>
+#include <rclc/timer.h>
+#include <rmw_microros/rmw_microros.h>
+
+#include <microros_transports.h>
+
+#include <geometry_msgs/msg/pose_stamped.h>
+#include <geometry_msgs/msg/twist.h>
+#include <sensor_msgs/msg/laser_scan.h>
+
+#include <math.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include <zephyr/net/net_event.h>
+#include <zephyr/net/net_if.h>
+#include <zephyr/net/wifi_mgmt.h>
+
+#include "pb_decode.h"
+#include "pb_encode.h"
+
+#include <zephyr/net/net_context.h>
+#include <zephyr/net/net_core.h>
+#include <zephyr/net/net_if.h>
+#include <zephyr/net/net_mgmt.h>
+
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/kernel.h>
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main, CONFIG_SENSOR_LOG_LEVEL);
 
-int main(void)
-{
-    const struct device *sensor;
+#define DHCP_OPTION_NTP (42)
 
+static uint8_t ntp_server[4];
+
+static struct net_mgmt_event_callback mgmt_cb;
+
+static struct net_dhcpv4_option_callback dhcp_cb;
+
+static const struct device *sensor;
+
+#define RCCHECK(fn)                                                                                \
+    {                                                                                              \
+        rcl_ret_t temp_rc = fn;                                                                    \
+        if ((temp_rc != RCL_RET_OK)) {                                                             \
+            printf("Failed status on line %d: %d. Aborting.\n", __LINE__, (int)temp_rc);           \
+            return 1;                                                                              \
+        }                                                                                          \
+    }
+#define RCSOFTCHECK(fn)                                                                            \
+    {                                                                                              \
+        rcl_ret_t temp_rc = fn;                                                                    \
+        if ((temp_rc != RCL_RET_OK)) {                                                             \
+            printf("Failed status on line %d: %d. Continuing.\n", __LINE__, (int)temp_rc);         \
+        }                                                                                          \
+    }
+
+rcl_publisher_t scan_publisher;
+rcl_subscription_t odom_subscriber;
+geometry_msgs__msg__Twist cmd_vel_twist_msg;
+
+// LED0
+#define LED0_NODE DT_ALIAS(led0)
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
+// Save timestamp of last message
+static uint64_t last_msg_timestamp;
+static uint64_t ros_timestamp;
+
+// Network management
+static bool connected = 0;
+
+static void start_dhcpv4_client(struct net_if *iface, void *user_data)
+{
+    ARG_UNUSED(user_data);
+
+    printk("Start on %s: index=%d", net_if_get_device(iface)->name, net_if_get_by_iface(iface));
+    net_dhcpv4_start(iface);
+}
+
+static void handler(struct net_mgmt_event_callback *cb, uint32_t mgmt_event, struct net_if *iface)
+{
+    int i = 0;
+
+    if (mgmt_event != NET_EVENT_IPV4_ADDR_ADD) {
+        return;
+    }
+
+    for (i = 0; i < NET_IF_MAX_IPV4_ADDR; i++) {
+        char buf[NET_IPV4_ADDR_LEN];
+
+        if (iface->config.ip.ipv4->unicast[i].addr_type != NET_ADDR_DHCP) {
+            continue;
+        }
+
+        printk("   Address[%d]: %s",
+                net_if_get_by_iface(iface),
+                net_addr_ntop(AF_INET,
+                        &iface->config.ip.ipv4->unicast[i].address.in_addr,
+                        buf,
+                        sizeof(buf)));
+        printk("    Subnet[%d]: %s",
+                net_if_get_by_iface(iface),
+                net_addr_ntop(AF_INET, &iface->config.ip.ipv4->netmask, buf, sizeof(buf)));
+        printk("    Router[%d]: %s",
+                net_if_get_by_iface(iface),
+                net_addr_ntop(AF_INET, &iface->config.ip.ipv4->gw, buf, sizeof(buf)));
+        printk("Lease time[%d]: %u seconds",
+                net_if_get_by_iface(iface),
+                iface->config.dhcpv4.lease_time);
+    }
+
+    connected = 1;
+}
+
+static void option_handler(struct net_dhcpv4_option_callback *cb,
+        size_t length,
+        enum net_dhcpv4_msg_type msg_type,
+        struct net_if *iface)
+{
+    char buf[NET_IPV4_ADDR_LEN];
+
+    printk("DHCP Option %d: %s", cb->option, net_addr_ntop(AF_INET, cb->data, buf, sizeof(buf)));
+}
+
+static void timer_callback(rcl_timer_t *timer, int64_t last_call_time)
+{
+    // Toggles LED0
+    gpio_pin_toggle_dt(&led);
+
+    struct sensor_value val[15];
+
+    // sensor_sample_fetch(sensor);
+    sensor_channel_get(sensor, SENSOR_CHAN_DISTANCE, val);
+
+    static sensor_msgs__msg__LaserScan scan;
+
+    scan.header.frame_id.data = "lds_01_link";
+
+    scan.header.stamp.sec = (int32_t)((ros_timestamp + k_uptime_get()) / 1000);
+    scan.header.stamp.nanosec = (uint32_t)((ros_timestamp + k_uptime_get()) % 1000) * 1000000;
+
+    scan.angle_min = val[1].val1 / 100 * 3.14 / 180;
+    scan.angle_max = val[1].val2 / 100 * 3.14 / 180;
+
+    scan.angle_increment = 0.8 * 3.14 / 180;
+    scan.time_increment = 0.1 / 540;
+    scan.scan_time = 0.1;
+    scan.range_min = 0.02;
+    scan.range_max = 12;
+
+    static float ranges[12];
+    static float intensities[12];
+
+    for (int i = 0; i < 12; i++) {
+        ranges[i] = (float)val[i + 2].val1 / 100.;
+        intensities[i] = (float)val[i + 2].val2 / 100.;
+    }
+
+    scan.ranges.data = ranges;
+    scan.intensities.data = intensities;
+    scan.ranges.size = 12;
+    scan.intensities.size = 12;
+
+    RCSOFTCHECK(rcl_publish(&scan_publisher, &scan, NULL));
+}
+
+int main()
+{
+    int ret;
+
+    printk("Starting micro-ROS Zephyr app\n");
+
+    // Init LED0
+    if (!gpio_is_ready_dt(&led)) {
+        return 0;
+    }
+
+    ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+    if (ret < 0) {
+        return 0;
+    }
+
+    // Initialize LiDAR
     sensor = DEVICE_DT_GET(DT_NODELABEL(lidar0));
     if (!device_is_ready(sensor)) {
         LOG_ERR("Sensor not ready");
         return 0;
     }
 
-    while (1) {
-        k_sleep(K_FOREVER);
+    // Init micro-ROS
+    static zephyr_transport_params_t agent_param = { { 0, 0, 0 }, "192.168.1.3", "8888" };
+
+    // Init micro-ROS
+    rmw_uros_set_custom_transport(MICRO_ROS_FRAMING_REQUIRED,
+            (void *)&agent_param,
+            zephyr_transport_open,
+            zephyr_transport_close,
+            zephyr_transport_write,
+            zephyr_transport_read);
+
+    net_mgmt_init_event_callback(&mgmt_cb, handler, NET_EVENT_IPV4_ADDR_ADD);
+    net_mgmt_add_event_callback(&mgmt_cb);
+
+    net_dhcpv4_init_option_callback(
+            &dhcp_cb, option_handler, DHCP_OPTION_NTP, ntp_server, sizeof(ntp_server));
+
+    net_dhcpv4_add_option_callback(&dhcp_cb);
+
+    net_if_foreach(start_dhcpv4_client, NULL);
+
+    while (!connected) {
+        // printf("Waiting for connection\n");
+        usleep(10000);
+    }
+    printf("Connection OK\n");
+
+    while (rmw_uros_ping_agent(100, 1) != RMW_RET_OK) {
+        printk("Waiting for agent...\n");
+        k_sleep(K_SECONDS(1)); // Sleep for 1 second
     }
 
-    return 0;
+    printk("Agent found!\n");
+
+    rcl_allocator_t allocator = rcl_get_default_allocator();
+    rclc_support_t support;
+
+    rcl_init_options_t init_options = rcl_get_zero_initialized_init_options();
+    RCCHECK(rcl_init_options_init(&init_options, allocator));
+    RCCHECK(rcl_init_options_set_domain_id(&init_options, CONFIG_ROS_ROS_DOMAIN_ID));
+
+    // Initialize micro-ROS with options
+    RCCHECK(rclc_support_init_with_options(&support, 0, NULL, &init_options, &allocator));
+
+    // Create node
+    rcl_node_t node;
+    RCCHECK(rclc_node_init_default(&node, "zephyr", "", &support));
+
+    // Create scan publisher
+    static rmw_qos_profile_t custom_qos_profile = rmw_qos_profile_sensor_data;
+    rclc_publisher_init(&scan_publisher,
+            &node,
+            ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, LaserScan),
+            "scan",
+            &custom_qos_profile);
+
+    // create timer
+    rcl_timer_t timer;
+    const unsigned int timer_timeout = 10;
+    RCCHECK(rclc_timer_init_default(&timer, &support, RCL_MS_TO_NS(timer_timeout), timer_callback));
+
+    // Create executor
+    rclc_executor_t executor = rclc_executor_get_zero_initialized_executor();
+    RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
+    RCCHECK(rclc_executor_add_timer(&executor, &timer));
+
+    // Synchronize time
+    RCCHECK(rmw_uros_sync_session(1000));
+    ros_timestamp = rmw_uros_epoch_millis();
+
+    rclc_executor_spin(&executor);
 }
