f4dfee2e6f433b9bd2edc8c2f460838c46ce7367
Clément Laigle <c.laigle@catie.fr>
2024-07-25 07:32:07 +0000

    Increase laser scan message points

--- app/lidar_example/src/main.c
+++ app/lidar_example/src/main.c
@@ -3,9 +3,10 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <zephyr/drivers/gpio.h>
-#include <zephyr/drivers/uart.h>
 #include <zephyr/kernel.h>
+
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/sensor.h>
 
 #include <rcl/error_handling.h>
 #include <rcl/rcl.h>
@@ -16,28 +17,15 @@
 
 #include <microros_transports.h>
 
-#include <geometry_msgs/msg/pose_stamped.h>
-#include <geometry_msgs/msg/twist.h>
 #include <sensor_msgs/msg/laser_scan.h>
-
-#include <math.h>
-#include <stdio.h>
-#include <unistd.h>
 
 #include <zephyr/net/net_event.h>
 #include <zephyr/net/net_if.h>
-#include <zephyr/net/wifi_mgmt.h>
-
-#include "pb_decode.h"
-#include "pb_encode.h"
 
 #include <zephyr/net/net_context.h>
 #include <zephyr/net/net_core.h>
 #include <zephyr/net/net_if.h>
 #include <zephyr/net/net_mgmt.h>
-
-#include <zephyr/drivers/sensor.h>
-#include <zephyr/kernel.h>
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main, CONFIG_SENSOR_LOG_LEVEL);
@@ -79,15 +67,12 @@ K_THREAD_DEFINE(uros_transport, 2048, uros_transport_task, NULL, NULL, NULL, 7, 
     }
 
 rcl_publisher_t scan_publisher;
-rcl_subscription_t odom_subscriber;
-geometry_msgs__msg__Twist cmd_vel_twist_msg;
 
 // LED0
 #define LED0_NODE DT_ALIAS(led0)
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
-// Save timestamp of last message
-static uint64_t last_msg_timestamp;
+// Save ROS timestamp
 static uint64_t ros_timestamp;
 
 // Network management
@@ -95,14 +80,21 @@ static bool connected = 0;
 static volatile bool agent_connected = false;
 
 // Struct contain ranges, intensities, start_angle, end_angle
-static struct lidar_data {
+struct lidar_data {
     float ranges[12];
     float intensities[12];
     float start_angle;
     float end_angle;
 };
 
-K_MSGQ_DEFINE(lidar_msgq, sizeof(struct lidar_data), 100, 4);
+#define NB_MSG 11
+#define NB_POINTS_PER_MSG 12
+#define NB_POINTS (NB_MSG * NB_POINTS_PER_MSG)
+
+static struct lidar_data lidar_msg[NB_MSG];
+int lidar_msg_index = 0;
+
+K_MSGQ_DEFINE(lidar_msgq, sizeof(struct lidar_data), 20, 4);
 
 static void start_dhcpv4_client(struct net_if *iface, void *user_data)
 {
@@ -157,24 +149,14 @@ static void option_handler(struct net_dhcpv4_option_callback *cb,
     printk("DHCP Option %d: %s", cb->option, net_addr_ntop(AF_INET, cb->data, buf, sizeof(buf)));
 }
 
-#define NB_MSG 11
-#define NB_POINTS_PER_MSG 12
-#define NB_POINTS (NB_MSG * NB_POINTS_PER_MSG)
-
-static struct lidar_data lidar_msg[NB_MSG];
-int lidar_msg_index = 0;
-
-extern void send_lidar_data(void *, void *, void *);
-
 void send_lidar_data(void *, void *, void *)
 {
     static float range_to_send[NB_POINTS];
     static float intensity_to_send[NB_POINTS];
-    // float start_angle_to_send, end_angle_to_send;
 
     struct lidar_data lidar_data;
 
-    static volatile sensor_msgs__msg__LaserScan scan;
+    static sensor_msgs__msg__LaserScan scan;
 
     while (true) {
         k_msgq_get(&lidar_msgq, &lidar_data, K_FOREVER);
@@ -245,55 +227,13 @@ void uros_transport_task()
     }
 }
 
-static void trigger_handler(const struct device *dev, struct sensor_trigger *trigger)
+static void trigger_handler(const struct device *dev, const struct sensor_trigger *trigger)
 {
-    // Blink LED
-
     struct sensor_value val[15];
-
-    static int index = 0;
-    static bool flag = false;
 
     struct lidar_data lidar_data;
 
     sensor_channel_get(sensor, SENSOR_CHAN_DISTANCE, val);
-
-    // printk("Start angle: %d, End angle: %d\n", val[1].val1, val[1].val2);
-
-    // if (index >= 120) {
-    //     flag = false;
-    //     index = 0;
-
-    //     lidar_data->end_angle = 13.; //val[1].val2 / 100 * 3.14 / 180;
-
-    //     // if(k_uptime_get() - last_msg_timestamp < 10) {
-    //     //     return;
-    //     // }
-    //     // send_lidar_data();
-    //     // k_mutex_lock(&buffer_mutex, K_FOREVER);
-    //     // k_work_submit(&lidar_send_data_work);
-
-    //     // k_work_submit_to_queue(&my_work_q, &lidar_send_data_work);
-    //     while (k_msgq_put(&lidar_msgq, lidar_data, K_NO_WAIT) != 0) {
-    //         printk("Failed to put data in queue\n");
-    //         k_msgq_purge(&lidar_msgq);
-    //     }
-    //     // k_msgq_put(&lidar_msgq, &lidar_data, K_NO_WAIT);
-
-    //     // last_msg_timestamp = k_uptime_get();
-    //     // rcl_publish(&scan_publisher, &scan, NULL);
-    // } else {
-    //     if (!flag) {
-    //         lidar_data->start_angle = val[1].val1 / 100 * 3.14 / 180;
-    //         flag = true;
-    //     }
-
-    //     for (int i = 0; i < 12; i++) {
-    //         lidar_data->ranges[index + i] = 12.;//(float)val[i + 2].val1 / 100.;
-    //         lidar_data->intensities[index + i] = 12.;//(float)val[i + 2].val2 / 100.;
-    //         index++;
-    //     }
-    // }
 
     lidar_data.start_angle = val[1].val1 / 100 * 3.14 / 180;
     lidar_data.end_angle = val[1].val2 / 100 * 3.14 / 180;
@@ -309,58 +249,14 @@ static void trigger_handler(const struct device *dev, struct sensor_trigger *tri
     }
 }
 
-static void timer_callback(rcl_timer_t *timer, int64_t last_call_time)
-{
-    // Toggles LED0
-    gpio_pin_toggle_dt(&led);
-
-    struct sensor_value val[15];
-
-    // sensor_sample_fetch(sensor);
-    sensor_channel_get(sensor, SENSOR_CHAN_DISTANCE, val);
-
-    // static sensor_msgs__msg__LaserScan scan;
-
-    // scan.header.frame_id.data = "lds_01_link";
-
-    // scan.header.stamp.sec = (int32_t)((ros_timestamp + k_uptime_get()) / 1000);
-    // scan.header.stamp.nanosec = (uint32_t)((ros_timestamp + k_uptime_get()) % 1000) * 1000000;
-
-    // scan.angle_min = val[1].val1 / 100 * 3.14 / 180;
-    // scan.angle_max = val[1].val2 / 100 * 3.14 / 180;
-
-    // scan.angle_increment = 0.8 * 3.14 / 180;
-    // scan.time_increment = 0.1 / 540;
-    // scan.scan_time = 0.1;
-    // scan.range_min = 0.02;
-    // scan.range_max = 12;
-
-    // for (int i = 0; i < 12; i++) {
-    //     ranges[i] = (float)val[i + 2].val1 / 100.;
-    //     intensities[i] = (float)val[i + 2].val2 / 100.;
-    // }
-
-    // start_angle = val[1].val1 / 100 * 3.14 / 180;
-    // end_angle = val[1].val2 / 100 * 3.14 / 180;
-
-    // k_work_submit(&lidar_send_data_work);
-
-    // scan.ranges.data = ranges;
-    // scan.intensities.data = intensities;
-    // scan.ranges.size = 12;
-    // scan.intensities.size = 12;
-
-    // RCSOFTCHECK(rcl_publish(&scan_publisher, &scan, NULL));
-}
-
 int main()
 {
     int ret;
 
     printk("Starting micro-ROS Zephyr app\n");
 
-    int priority = 5; // Set your thread's priority
-    k_tid_t thread_id = k_thread_create(&thread_data,
+    int priority = 5;
+    k_thread_create(&thread_data,
             thread_stack,
             K_THREAD_STACK_SIZEOF(thread_stack),
             send_lidar_data,
@@ -417,14 +313,15 @@ int main()
     net_if_foreach(start_dhcpv4_client, NULL);
 
     while (!connected) {
-        // printf("Waiting for connection\n");
-        usleep(10000);
+        printf("Waiting for connection\n");
+        k_sleep(K_SECONDS(1));
     }
+
     printf("Connection OK\n");
 
     while (rmw_uros_ping_agent(100, 1) != RMW_RET_OK) {
         printk("Waiting for agent...\n");
-        k_sleep(K_SECONDS(1)); // Sleep for 1 second
+        k_sleep(K_SECONDS(1));
     }
 
     agent_connected = true;
@@ -453,24 +350,13 @@ int main()
             "scan",
             &custom_qos_profile);
 
-    // create timer
-    rcl_timer_t timer;
-    // const unsigned int timer_timeout = 10;
-    // RCCHECK(rclc_timer_init_default(&timer, &support, RCL_MS_TO_NS(timer_timeout),
-    // timer_callback));
-
     // Create executor
     executor = rclc_executor_get_zero_initialized_executor();
     RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
-    // RCCHECK(rclc_executor_add_timer(&executor, &timer));
 
     // Synchronize time
     RCCHECK(rmw_uros_sync_session(1000));
     ros_timestamp = rmw_uros_epoch_millis();
 
-    while (1) {
-        k_msleep(1000);
-    }
-
-    // rclc_executor_spin(&executor);
+    k_sleep(K_FOREVER);
 }
