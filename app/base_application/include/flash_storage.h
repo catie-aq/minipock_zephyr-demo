4c5d3db71db6764d089325049daa801bf2a9eb9e
Clément Laigle <c.laigle@catie.fr>
2025-01-14 10:25:12 +0000

    Use flash API instead of NVS

--- app/base_application/include/flash_storage.h
+++ app/base_application/include/flash_storage.h
@@ -1,8 +1,14 @@
 #ifndef FLASH_STORAGE_H
 #define FLASH_STORAGE_H
 
+#define NAMESPACE 0
+#define SSID 1
+#define PASSWORD 2
+#define CHANNEL 3
+#define AGENT_IP 4
+
 int flash_storage_init(void);
-int flash_storage_write(const char *key, const char *value);
-int flash_storage_read(const char *key, char *value, size_t max_len);
+int flash_storage_write(const int key, const char *value, size_t len);
+int flash_storage_read(const int key, char *value, size_t max_len);
 
 #endif // FLASH_STORAGE_H
--- app/base_application/src/settings_cmd.c
+++ app/base_application/src/settings_cmd.c
@@ -2,32 +2,92 @@
 
 #include "flash_storage.h"
 
-static int cmd_flash_storage_read(const struct shell *shell, size_t argc, char **argv)
+static int cmd_set_wifi_ssid(const struct shell *shell, size_t argc, char **argv)
 {
-    if (argc != 2) {
-        shell_print(shell, "Usage: %s <address>", argv[0]);
-        return -1;
+    int ret = -1;
+
+    if (argc > 2) {
+        shell_print(shell, "Usage: %s [ssid]", argv[0]);
+        return ret;
+    } else if (argc == 1) {
+        char ssid[32];
+        ret = flash_storage_read(SSID, ssid, sizeof(ssid));
+        shell_print(shell, "SSID: %s", ssid);
+        return 0;
+    } else {
+        ret = flash_storage_write(SSID, argv[1], strlen(argv[1]));
+        shell_print(shell, "Set SSID: %s", argv[1]);
     }
 
-    uint32_t address = strtoul(argv[1], NULL, 0);
-    uint32_t data;
-    flash_storage_read(address, &data, sizeof(data));
-    shell_print(shell, "Read data: 0x%08x", data);
-
-    return 0;
+    return ret;
 }
 
-static int cmd_flash_storage_write(const struct shell *shell, size_t argc, char **argv)
+static int cmd_set_wifi_password(const struct shell *shell, size_t argc, char **argv)
 {
-    if (argc != 3) {
-        shell_print(shell, "Usage: %s <address> <data>", argv[0]);
-        return -1;
+    int ret = -1;
+
+    if (argc > 2) {
+        shell_print(shell, "Usage: %s [password]", argv[0]);
+        return ret;
+    } else if (argc == 1) {
+        char password[32];
+        ret = flash_storage_read(PASSWORD, password, sizeof(password));
+        shell_print(shell, "Password: %s", password);
+        return 0;
+    } else {
+        ret = flash_storage_write(PASSWORD, argv[1], strlen(argv[1]));
+        shell_print(shell, "Set password: %s", argv[1]);
     }
 
-    uint32_t address = strtoul(argv[1], NULL, 0);
-    uint32_t data = strtoul(argv[2], NULL, 0);
-    flash_storage_write(address, data);
-    shell_print(shell, "Write data: 0x%08x", data);
-
-    return 0;
+    return ret;
 }
+
+static int cmd_set_wifi_channel(const struct shell *shell, size_t argc, char **argv)
+{
+    int ret = -1;
+
+    if (argc > 2) {
+        shell_print(shell, "Usage: %s [channel]", argv[0]);
+        return ret;
+    } else if (argc == 1) {
+        uint8_t channel;
+        ret = flash_storage_read(CHANNEL, &channel, sizeof(channel));
+        shell_print(shell, "Channel: %d", channel);
+        return 0;
+    } else {
+        uint8_t channel = (uint8_t)strtol(argv[1], NULL, 10);
+        ret = flash_storage_write(CHANNEL, &channel, sizeof(channel));
+        shell_print(shell, "Set channel: %d", channel);
+    }
+
+    return ret;
+}
+
+static int cmd_set_namespace(const struct shell *shell, size_t argc, char **argv)
+{
+    int ret = -1;
+
+    if (argc > 2) {
+        shell_print(shell, "Usage: %s [namespace]", argv[0]);
+        return ret;
+    } else if (argc == 1) {
+        char namespace[32];
+        ret = flash_storage_read(NAMESPACE, namespace, sizeof(namespace));
+        shell_print(shell, "Namespace: %s", namespace);
+        return 0;
+    } else {
+        ret = flash_storage_write(NAMESPACE, argv[1], strlen(argv[1]));
+        shell_print(shell, "Set namespace: %s", argv[1]);
+    }
+
+    return ret;
+}
+
+SHELL_STATIC_SUBCMD_SET_CREATE(settings,
+        SHELL_CMD(ssid, NULL, "Set SSID", cmd_set_wifi_ssid),
+        SHELL_CMD(password, NULL, "Set password", cmd_set_wifi_password),
+        SHELL_CMD(channel, NULL, "Set channel", cmd_set_wifi_channel),
+        SHELL_CMD(namespace, NULL, "Set namespace", cmd_set_namespace),
+        SHELL_SUBCMD_SET_END);
+
+SHELL_CMD_REGISTER(settings, &settings, "Settings commands", NULL);
