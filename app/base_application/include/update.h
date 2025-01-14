615ddbbf5490560b00844c1869651d3c08fd7ac3
Clément Laigle <c.laigle@catie.fr>
2025-01-14 15:58:05 +0000

    Fix current version reading

--- app/base_application/include/update.h
+++ app/base_application/include/update.h
@@ -1,6 +1,13 @@
 #ifndef UPDATE_H
 #define UPDATE_H
 
+#include <zephyr/storage/flash_map.h>
+
+#define UPDATE_CHUNK_SIZE 1024
+
+#define PRIMARY_SLOT_PARTITION_ID FIXED_PARTITION_ID(slot0_partition)
+#define SECOND_SLOT_PARTITION_ID FIXED_PARTITION_ID(slot1_partition)
+
 struct update_interface {
     void (*request_update_params)(void);
     void (*request_update)(void);
